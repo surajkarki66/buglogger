9a41edde3a02397488eaf76949325f7222d43788
surajkarki66 <suraj.karki500@protonmail.com>
2021-10-14 19:12:38 +0545

    Created table container

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+import LogItem from "./LogItem";
 
 const App = () => {
+  const [logs, setLogs] = useState([
+    {
+      _id: 1,
+      text: "This is log one",
+      priority: "low",
+      user: "Suraj",
+      created: new Date().toString(),
+    },
+    {
+      _id: 2,
+      text: "This is log two",
+      priority: "moderate",
+      user: "Ram",
+      created: new Date().toString(),
+    },
+    {
+      _id: 3,
+      text: "This is log three",
+      priority: "high",
+      user: "Denver",
+      created: new Date().toString(),
+    },
+  ]);
   return (
-    <div className="app">
-      <h1>BugTracker</h1>
-    </div>
+    <Container>
+      <Table>
+        <thead>
+          <tr>
+            <th>Priority</th>
+            <th>Log Text</th>
+            <th>User</th>
+            <th>Created</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {" "}
+          {logs.map((log) => (
+            <LogItem log={log} key={log._id} />
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   );
 };
 
